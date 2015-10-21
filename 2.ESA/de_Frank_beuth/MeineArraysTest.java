e9e56dd4fccdc0cd2be0e9b003c32c4786da2821
Norman.Frank <norm_an2000@yahoo.de>
2015-10-21 11:12:55 +0200

    Add JUnit test asserts as requested by Task 2

--- 2.ESA/de_Frank_beuth/MeineArraysTest.java
+++ 2.ESA/de_Frank_beuth/MeineArraysTest.java
@@ -1,16 +1,60 @@
 package de_Frank_beuth;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by Norman Frank on 20.10.15.
  */
 
 public class MeineArraysTest {
-
-    public static void main (String[] args) {
+	
+	private MeineArrays testArray;
+	
+	@Before
+	public void init() {
+	testArray = new MeineArrays();
+	}
+	
+	@Test
+	public void testNull() {
+		int[] ar = new int [0];
+		int result = testArray.binarySearchRecursiv(ar, 1);
+		assertEquals(-1, result);
+		assertEquals(1, testArray.getcallCount());
+	}
+	
+	@Test
+	public void testEinElement() {
+		int[] ar = {3};
+		int result = testArray.binarySearchRecursiv(ar, 1);
+		assertEquals(0, result);
+		assertEquals(1, testArray.getcallCount());
+	}
+	
+	@Test
+    public void testMehrereElemente() {
         int[] ar = {3, 5, 6, 9, 13, 18, 34, 46};
-        int result = MeineArrays.binarySearchRecursiv(ar, 13);
-        System.out.println("Anzahl der Durchgänge: "+ MeineArrays.getcallCount());
-        System.out.println("Gesuchter Wert: "+ result);
+        int result = testArray.binarySearchRecursiv(ar, 9);
+		assertEquals(8, result);
+		assertEquals(1, testArray.getcallCount());	
+    }
+	
+	@Test
+    public void testElementeAmAnfang() {
+        int[] ar = {3, 5, 6, 9, 13, 18, 34, 46};
+        int result = testArray.binarySearchRecursiv(ar, 3);
+		assertEquals(8, result);
+		assertEquals(1, testArray.getcallCount());	
+    }
+	
+	@Test
+    public void testElementeAmEnde() {
+        int[] ar = {3, 5, 6, 9, 13, 18, 34, 46};
+        int result = testArray.binarySearchRecursiv(ar, 46);
+		assertEquals(8, result);
+		assertEquals(1, testArray.getcallCount());	
     }
 }
